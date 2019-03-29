5cfb0af4c98e6ed6b285f8c03952775bae2c7eba
DATBOI <alexsimons9999@gmail.com>
2019-03-29 18:21:43 -0500

    Basic Hello World!

---
+++ src/main/kotlin/io/acari/Application.kt
@@ -0,0 +1,7 @@
+package io.acari
+
+class Application
+
+fun main(args: Array<String>){
+    println("Hello World!")
+}
---
+++ src/test/kotlin/io/acari/ApplicationKtTest.kt
@@ -0,0 +1,13 @@
+package io.acari
+
+import org.assertj.core.api.Assertions
+import org.junit.Assert.*
+import org.junit.Test
+
+class ApplicationKtTest {
+
+    @Test
+    fun main() {
+        Assertions.assertThat(true).isTrue()
+    }
+}
