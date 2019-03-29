e1c0565a1a364dc7b2f9b6dbdfc09e373df345cf
DATBOI <alexsimons9999@gmail.com>
2019-03-29 18:35:06 -0500

    Used the quick start, prolly should have lead with that

---
+++ src/main/kotlin/io/acari/MainVerticle.kt
@@ -0,0 +1,27 @@
+package io.acari
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.Future
+
+class MainVerticle : AbstractVerticle() {
+
+  override fun start(startFuture: Future<Void>) {
+    vertx
+      .createHttpServer()
+      .requestHandler { req ->
+        req.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!")
+      }
+      .listen(8888) { http ->
+        if (http.succeeded()) {
+          startFuture.complete()
+          println("HTTP server started on port 8888")
+        } else {
+          startFuture.fail(http.cause())
+        }
+      }
+
+  }
+
+}
