f8fc8acdc211cde369052bed8a249645e46c57ce
DATBOI <alexsimons9999@gmail.com>
2019-03-30 12:16:13 -0500

    Start of oauth, looks like I will have to do manual management

--- src/main/kotlin/io/acari/MainVerticle.kt
+++ src/main/kotlin/io/acari/MainVerticle.kt
@@ -2,10 +2,25 @@ package io.acari
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.ext.auth.oauth2.OAuth2ClientOptions
+import io.vertx.ext.auth.oauth2.providers.OpenIDConnectAuth
 
 class MainVerticle : AbstractVerticle() {
 
   override fun start(startFuture: Future<Void>) {
+    OpenIDConnectAuth.discover(
+      vertx, OAuth2ClientOptions()
+        .setSite("http://pringle:8080/auth/realms/master")
+        .setClientID("sogos")
+        .setAuthorizationPath("http://pringle:8080/auth/realms/master/protocol/openid-connect/auth")
+    ) { result ->
+      if (result.succeeded()) {
+        result.map {oauth2 ->
+          println(oauth2.flowType)
+        }
+      }
+    }
+
     vertx
       .createHttpServer()
       .requestHandler { req ->
