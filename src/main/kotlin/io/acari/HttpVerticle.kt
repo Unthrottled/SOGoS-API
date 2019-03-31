22f3ff849005686e996c91d92cd51b19f9d57726
DATBOI <alexsimons9999@gmail.com>
2019-03-30 20:05:41 -0500

    OpenID Connect Works!! ᕙ(⇀‸↼‶)ᕗ

--- src/main/kotlin/io/acari/HttpVerticle.kt
+++ src/main/kotlin/io/acari/HttpVerticle.kt
@@ -2,6 +2,7 @@ package io.acari
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.ext.auth.oauth2.AccessToken
 import io.vertx.ext.auth.oauth2.OAuth2ClientOptions
 import io.vertx.ext.auth.oauth2.providers.OpenIDConnectAuth
 import io.vertx.ext.web.Router
@@ -10,8 +11,6 @@ import io.vertx.ext.web.handler.OAuth2AuthHandler
 import io.vertx.ext.web.handler.SessionHandler
 import io.vertx.ext.web.handler.UserSessionHandler
 import io.vertx.ext.web.sstore.LocalSessionStore
-import io.vertx.kotlin.core.json.json
-import io.vertx.kotlin.core.json.obj
 
 class HttpVerticle : AbstractVerticle() {
   override fun start(startFuture: Future<Void>) {
@@ -19,6 +18,7 @@ class HttpVerticle : AbstractVerticle() {
       vertx, OAuth2ClientOptions()
         .setSite("http://pringle:8080/auth/realms/master")
         .setClientID("sogos")
+        .setClientSecret(System.getenv("sogos.client.secret"))
     ) { result ->
       if (result.succeeded()) {
         result.map { oauth2 ->
@@ -29,25 +29,21 @@ class HttpVerticle : AbstractVerticle() {
             .handler(SessionHandler.create(LocalSessionStore.create(vertx)))
             .handler(UserSessionHandler.create(oauth2))
             .handler(UserSessionHandler.create(oauth2))
-            .handler(OAuth2AuthHandler.create(oauth2)
+            .handler(OAuth2AuthHandler.create(oauth2, "http://pringle:8888/engage")
               .setupCallback(authEngagmentRoute)
-              .extraParams(
-                json {
-                  obj("redirect_uri" to "http://localhost:8888/engage")
-                }
-              )
-              .addAuthorities(setOf("profile", "openid"))
+              .addAuthorities(setOf("profile", "openid", "email"))
             )
           router.get("/")
             .handler { req ->
+              val user = req.user() as AccessToken
               req.response()
                 .putHeader("content-type", "text/plain")
-                .end("Hello from Vert.x: ${req.user()}!")
+                .end("Hello from Vert.x: ${user.idToken()}!")
             }
 
           vertx
             .createHttpServer()
-            .requestHandler { request -> router.accept(request) }
+            .requestHandler(router)
             .listen(8888) {
               if(it.succeeded()){
                 startFuture.complete()
