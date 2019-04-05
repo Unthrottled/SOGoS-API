95d3d43d239c81728d27c04917c96c29a58bcf58
DATBOI <alexsimons9999@gmail.com>
2019-04-05 04:58:29 -0500

    Updated user endpoint to not make http calls

--- src/main/kotlin/io/acari/http/APIRouter.kt
+++ src/main/kotlin/io/acari/http/APIRouter.kt
@@ -1,26 +1,45 @@
 package io.acari.http
 
 import io.acari.util.toOptional
+import io.reactivex.Maybe
+import io.reactivex.functions.BiFunction
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.oauth2.AccessToken
 import io.vertx.ext.web.Router
-import io.vertx.reactivex.SingleHelper
+
+object UserService {
+  fun createUser(accessToken: AccessToken): Maybe<String> =
+    Maybe.just(accessToken.accessToken())
+      .zipWith(
+        Maybe.just(accessToken.idToken()),
+        BiFunction<JsonObject, JsonObject, Pair<JsonObject, JsonObject>> { t1, t2 -> Pair(t1, t2) })
+      .map {
+        val idToken = it.second
+        JsonObject()
+          .put("fullName", idToken.getValue("name"))
+          .put("userName", idToken.getValue("preferred_username"))
+          .put("firstName", idToken.getValue("given_name"))
+          .put("lastName", idToken.getValue("family_name"))
+          .put("email", idToken.getValue("email"))
+          .encodePrettily()
+      }
+
+}
 
 fun mountAPIRoute(router: Router): Router =
   router.toOptional()
     .map {
       it.get("/user")
-        .handler{ request ->
+        .handler { request ->
           val accessToken = request.user() as AccessToken
-          SingleHelper.toSingle<JsonObject> {
-            accessToken.userInfo(it) // todo: probably can use the id token
-          }.subscribe({ userInfo ->
-            request.response()
-              .putHeader("content-type", "application/json")
-              .end(userInfo.encodePrettily())
-          }){
-            request.fail(404)
-          }
+          UserService.createUser(accessToken)
+            .subscribe({
+              request.response()
+                .putHeader("content-type", "application/json")
+                .end(it)
+            }) {
+              request.fail(404)
+            }
         }
 
       it.get("/")
@@ -28,13 +47,17 @@ fun mountAPIRoute(router: Router): Router =
           val user = req.user() as AccessToken
           req.response()
             .putHeader("content-type", "text/plain")
-            .end("""
-                |Hello from Vert.x: ${user.idToken()}!
+            .end(
+              """
+                |Hello from Vert.x:
                 |
-                |${user.accessToken()}
+                |${user.idToken().encodePrettily()}
                 |
-                |${user.refreshToken()}
-            """.trimMargin())
+                |${user.accessToken().encodePrettily()}
+                |
+                |${user.refreshToken().encodePrettily()}
+            """.trimMargin()
+            )
         }
       it
     }.orElse(router)
