d527774cda6276c42bca5989160c71c68210f168
DATBOI <alexsimons9999@gmail.com>
2019-04-09 07:31:51 -0500

    So oauth2token is access token

--- src/main/kotlin/io/acari/http/APIRouter.kt
+++ src/main/kotlin/io/acari/http/APIRouter.kt
@@ -10,6 +10,7 @@ import io.vertx.ext.auth.User
 import io.vertx.ext.auth.oauth2.AccessToken
 import io.vertx.ext.auth.oauth2.impl.OAuth2TokenImpl
 import io.vertx.ext.web.Router
+import io.vertx.reactivex.MaybeHelper
 import io.vertx.reactivex.SingleHelper
 
 object UserService {
@@ -19,29 +20,33 @@ object UserService {
         .map { it as AccessToken }
         .flatMap {     accessToken ->
           createUserFromAccessToken(accessToken)
-        }.switchIfEmpty(createUserFromOAuthToken(user))
-
-  private fun createUserFromOAuthToken(user: User): Maybe<String> =
-    Maybe.just(user)
-      .filter { it is OAuth2TokenImpl }
-      .map { it as OAuth2TokenImpl }
-      .map { "" }
+        }
 
   private fun createUserFromAccessToken(accessToken: AccessToken): Maybe<String>? {
     return Maybe.just(accessToken.accessToken())
       .zipWith(
-        Maybe.just(accessToken.idToken()),
+        accessToken.idToken().toOptional()
+          .map { Maybe.just(it) }
+          .orElseGet {
+            MaybeHelper.toMaybe {
+              accessToken.userInfo(it)
+            }
+          },
         BiFunction<JsonObject, JsonObject, Pair<JsonObject, JsonObject>> { t1, t2 -> Pair(t1, t2) })
       .map {
         val idToken = it.second
-        JsonObject()
-          .put("fullName", idToken.getValue("name"))
-          .put("userName", idToken.getValue("preferred_username"))
-          .put("firstName", idToken.getValue("given_name"))
-          .put("lastName", idToken.getValue("family_name"))
-          .put("email", idToken.getValue("email"))
-          .encodePrettily()
+        extractUser(idToken)
       }
+  }
+
+  private fun extractUser(idToken: JsonObject): String? {
+    return JsonObject()
+      .put("fullName", idToken.getValue("name"))
+      .put("userName", idToken.getValue("preferred_username"))
+      .put("firstName", idToken.getValue("given_name"))
+      .put("lastName", idToken.getValue("family_name"))
+      .put("email", idToken.getValue("email"))
+      .encodePrettily()
   }
 }
 
@@ -93,6 +98,7 @@ fun createAPIRoute(vertx: Vertx): Router {
             .putHeader("content-type", "application/json")
             .end(it)
         }){
+          it.printStackTrace()
           request.fail(404)
         }
     }
