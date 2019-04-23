de2161312fa8f0c03e0a9fdb860872c22be9f2d8
DATBOI <alexsimons9999@gmail.com>
2019-04-23 04:37:34 -0500

    Finished the POKO Codec

--- src/main/kotlin/io/acari/util/POKOCodec.kt
+++ src/main/kotlin/io/acari/util/POKOCodec.kt
@@ -4,28 +4,32 @@ import io.vertx.core.buffer.Buffer
 import io.vertx.core.eventbus.MessageCodec
 import io.vertx.core.json.JsonObject
 
-open class POKOCodec<T>(private val seralizer: (JsonObject) -> Unit) : MessageCodec<T, T> {
-  override fun decodeFromWire(pos: Int, buffer: Buffer?): T {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+const val INT_LENGTH = 4
+
+open class POKOCodec<T>(private val serializer: (JsonObject) -> JsonObject,
+                        private val deserializer: (JsonObject) -> T,
+                        private val name: String) : MessageCodec<T, T> {
+  override fun decodeFromWire(pos: Int, buffer: Buffer): T {
+    val messageLength = buffer.getInt(pos)
+    val startPosition = pos + INT_LENGTH
+    val endPosition = startPosition + messageLength
+    val jsonString = buffer.getString(startPosition, endPosition)
+    val jsonObject = JsonObject(jsonString)
+    return deserializer(jsonObject)
   }
 
   override fun systemCodecID(): Byte = -1
 
   override fun encodeToWire(buffer: Buffer, s: T) {
     val jsonToSend = JsonObject()
-    seralizer(jsonToSend)
-    val jsonMessage = jsonToSend.encode().toByteArray()
+    val jsonMessage = serializer(jsonToSend).encode().toByteArray()
     val messageLength = jsonMessage.size
     buffer.appendInt(messageLength)
     buffer.appendBytes(jsonMessage)
   }
 
-  override fun transform(s: T): T {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun transform(t: T): T = t
 
-  override fun name(): String {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun name(): String = name
 
 }
