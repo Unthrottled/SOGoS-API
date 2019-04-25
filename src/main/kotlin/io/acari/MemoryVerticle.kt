73799ab85918ba81277e38a15e98a821b05b2b11
DATBOI <alexsimons9999@gmail.com>
2019-04-25 06:19:23 -0500

    More able to remember things now

--- src/main/kotlin/io/acari/MemoryVerticle.kt
+++ src/main/kotlin/io/acari/MemoryVerticle.kt
@@ -1,6 +1,8 @@
 package io.acari
 
 import io.acari.util.loggerFor
+import io.reactivex.Completable
+import io.reactivex.Observer
 import io.vertx.core.Future
 import io.vertx.reactivex.core.AbstractVerticle
 import io.vertx.reactivex.ext.mongo.MongoClient
@@ -8,17 +10,21 @@ import io.vertx.reactivex.ext.mongo.MongoClient
 class MemoryVerticle : AbstractVerticle() {
   companion object {
     private val logger = loggerFor(MemoryVerticle::class.java)
-    init {
-//        System.setProperty("org.mongodb.async.type","netty")
-    }
   }
 
   override fun start(startFuture: Future<Void>) {
     val memoryConfiguration = config().getJsonObject("memory")
     val mongoClient = MongoClient.createShared(vertx, memoryConfiguration)
-    mongoClient.rxGetCollections()
+    mongoClient.rxCreateCollection("test")
+      .onErrorResumeNext { error->
+        if(error.message?.contains("already exists") == true){
+          Completable.complete()
+        } else {
+          Completable.error(error)
+        }
+      }
+      .andThen(mongoClient.rxGetCollections())
       .subscribe({
-        println(it)
         startFuture.complete()
       }){
         startFuture.fail(it)
