53d43c284a56c862893435b332d4f201fecedfbb
DATBOI <alexsimons9999@gmail.com>
2019-04-28 17:06:43 -0500

    Less need for static content serving

--- src/main/kotlin/io/acari/http/APIRouter.kt
+++ src/main/kotlin/io/acari/http/APIRouter.kt
@@ -1,16 +1,9 @@
 package io.acari.http
 
-import io.acari.developer.createStaticContentProxy
 import io.acari.util.loggerFor
-import io.acari.util.toOptional
-import io.reactivex.Maybe
-import io.vertx.core.Handler
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
-import io.vertx.ext.auth.oauth2.AccessToken
 import io.vertx.ext.web.Router
-import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.StaticHandler
 
 private val logger = loggerFor("APIRouter")
 
@@ -19,10 +12,9 @@ fun mountAPIRoute(vertx: Vertx, router: Router, configuration: JsonObject): Rout
 
   // Static content path must be mounted last, as a fall back
   router.get("/*")
-    .handler(fetchStaticContentHandler(vertx, configuration))
     .failureHandler { routingContext ->
       val statusCode = routingContext.statusCode()
-      if(statusCode !=401 && statusCode != 403){
+      if(statusCode != 401 && statusCode != 403){
         routingContext.reroute("/")
       } else {
         routingContext.response().setStatusCode(404).end()
@@ -32,11 +24,6 @@ fun mountAPIRoute(vertx: Vertx, router: Router, configuration: JsonObject): Rout
   return router
 }
 
-fun fetchStaticContentHandler(vertx: Vertx, configuration: JsonObject): Handler<RoutingContext> =
-  createStaticContentProxy(vertx, configuration)
-    .orElseGet {
-      StaticHandler.create()
-    }
 
 fun createAPIRoute(vertx: Vertx): Router {
   val router = Router.router(vertx)
