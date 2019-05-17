60163cd1a3b8c81ac8ba3087dcefc1b3d8dedca1
DATBOI <alexsimons9999@gmail.com>
2019-05-17 05:13:40 -0500

    Bulk upload, needs to be fleshed out more

--- src/main/kotlin/io/acari/http/ActivityRoutes.kt
+++ src/main/kotlin/io/acari/http/ActivityRoutes.kt
@@ -18,12 +18,32 @@ const val STARTED_ACTIVITY = "STARTED_ACTIVITY"
 
 fun createActivityRoutes(vertx: Vertx): Router {
   val router = router(vertx)
+  router.post("/bulk").handler { requestContext ->
+    val bodyAsJsonArray = requestContext.bodyAsJsonArray
+    bodyAsJsonArray.stream()
+      .map { it as JsonObject }
+      .forEach { activity ->
+        //todo: some type of sanitization?
+        vertx.eventBus().publish(EFFECT_CHANNEL,
+          Effect(
+            activity.getString("guid"),
+            Instant.now().toEpochMilli(),
+            activity.getLong("antecedenceTime"),
+            STARTED_ACTIVITY,
+            activity.getJsonObject("activity"),
+            extractValuableHeaders(requestContext)
+          )
+        )
+      }
+    requestContext.response().setStatusCode(200).end()
+  }
   router.post("/start").handler { requestContext ->
     val bodyAsJson = requestContext.bodyAsJson
+    val timeCreated = Instant.now().toEpochMilli()
     vertx.eventBus().publish(EFFECT_CHANNEL, Effect(
       bodyAsJson.getString("guid"),
-      Instant.now().toEpochMilli(),
-      bodyAsJson.getLong("antecedenceTime"),
+      timeCreated,
+      timeCreated,
       STARTED_ACTIVITY,
       bodyAsJson.getJsonObject("activity"),
       extractValuableHeaders(requestContext)
