aab068a116d9c645be33ea7f73558c193073f598
DATBOI <alexsimons9999@gmail.com>
2019-06-17 06:02:47 -0500

    Cool it saves!

--- src/main/kotlin/io/acari/memory/activity/ActivityEffectListener.kt
+++ src/main/kotlin/io/acari/memory/activity/ActivityEffectListener.kt
@@ -1,12 +1,16 @@
 package io.acari.memory.activity
 
 import io.acari.http.STARTED_ACTIVITY
+import io.acari.memory.ActivityHistorySchema
 import io.acari.memory.CurrentActivitySchema
 import io.acari.memory.Effect
 import io.acari.memory.user.UserMemoryWorkers
 import io.reactivex.Completable
 import io.reactivex.CompletableSource
+import io.reactivex.Single
 import io.vertx.core.Handler
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
 import io.vertx.ext.mongo.UpdateOptions
 import io.vertx.kotlin.core.json.jsonObjectOf
 import io.vertx.reactivex.core.Vertx
@@ -18,31 +22,33 @@ class ActivityEffectListener(private val mongoClient: MongoClient, private val v
   override fun handle(message: Message<Effect>) {
     val effect = message.body()
     writeCurrentActivity(effect)
-      .andThen(writeActivityLog(effect))
+      .flatMapCompletable { activity -> writeActivityLog(activity) }
       .subscribe({}) {
         UserMemoryWorkers.log.warn("Unable to save user for reasons.", it)
       }
   }
 
-  private fun writeActivityLog(effect: Effect): CompletableSource {
-    return Completable.complete()
+  private fun writeActivityLog(activity: JsonObject): CompletableSource {
+    return mongoClient.rxInsert(ActivityHistorySchema.COLLECTION, activity)
+      .ignoreElement()
   }
 
-  private fun writeCurrentActivity(effect: Effect): Completable =
-    if (isActivity(effect) && shouldTime(effect)) {
+  private fun writeCurrentActivity(effect: Effect): Single<JsonObject> {
+    val activity = jsonObjectOf(
+      CurrentActivitySchema.GLOBAL_USER_IDENTIFIER to effect.guid,
+      CurrentActivitySchema.CONTENT to effect.content,
+      CurrentActivitySchema.TIME_OF_ANTECEDENCE to effect.antecedenceTime
+    )
+    return if (isActivity(effect) && shouldTime(effect)) {
       mongoClient.rxReplaceDocumentsWithOptions(
         CurrentActivitySchema.COLLECTION,
         jsonObjectOf(CurrentActivitySchema.GLOBAL_USER_IDENTIFIER to effect.guid),
-        jsonObjectOf(
-          CurrentActivitySchema.GLOBAL_USER_IDENTIFIER to effect.guid,
-          CurrentActivitySchema.CONTENT to effect.content,
-          CurrentActivitySchema.TIME_OF_ANTECEDENCE to effect.antecedenceTime
-        ), UpdateOptions(true)
-      )
-        .ignoreElement()
+        activity, UpdateOptions(true)
+      ).map { activity }
     } else {
-      Completable.complete()
+      Single.just(activity)
     }
+  }
 
   private fun shouldTime(effect: Effect): Boolean =
     when (effect.content.getString("type") ?: "PASSIVE") {
