1b9f4ad7abb01cd2aadf2337520a0228c760ef2c
DATBOI <alexsimons9999@gmail.com>
2019-06-18 04:39:49 -0500

    More path params and setting response headers

--- src/main/kotlin/io/acari/http/HistoryRoutes.kt
+++ src/main/kotlin/io/acari/http/HistoryRoutes.kt
@@ -1,39 +1,40 @@
 package io.acari.http
 
 import io.acari.memory.ActivityHistorySchema
-import io.acari.memory.activity.CURRENT_ACTIVITY_CHANNEL
-import io.acari.memory.activity.CurrentActivityRequest
-import io.acari.memory.activity.CurrentActivityResponse
-import io.acari.security.USER_IDENTIFIER
+import io.acari.memory.activity.activityFromJson
 import io.acari.util.loggerFor
 import io.netty.handler.codec.http.HttpHeaderNames
-import io.netty.handler.codec.http.HttpHeaderValues
 import io.vertx.core.Vertx
-import io.vertx.core.eventbus.Message
 import io.vertx.core.json.Json
 import io.vertx.ext.web.Router
 import io.vertx.kotlin.core.json.jsonObjectOf
-import io.vertx.reactivex.SingleHelper
 import io.vertx.reactivex.ext.mongo.MongoClient
 
 private val logger = loggerFor("History Routes")
 
+const val JSON_STREAM = "application/stream+json"
+
 fun createHistoryRoutes(vertx: Vertx, mongoClient: MongoClient): Router {
   val router = Router.router(vertx)
-  //todo: path params
-  router.get("/feed").handler { requestContext ->
-    val userIdentifier = requestContext.request().headers().get(USER_IDENTIFIER)
+  router.get("/:userIdentifier/feed").handler { requestContext ->
+    val userIdentifier = requestContext.request().getParam("userIdentifier")
     val response = requestContext.response()
     response.isChunked = true
-    mongoClient.findBatch(ActivityHistorySchema.COLLECTION, jsonObjectOf(
-      ActivityHistorySchema.GLOBAL_USER_IDENTIFIER to userIdentifier
-    )).toFlowable()
+    response.putHeader(HttpHeaderNames.CONTENT_TYPE, JSON_STREAM)
+    mongoClient.findBatch(
+      ActivityHistorySchema.COLLECTION, jsonObjectOf(
+        ActivityHistorySchema.GLOBAL_USER_IDENTIFIER to userIdentifier
+      )
+    ).toFlowable()
+      .map {
+        Json.encode(activityFromJson(it))
+      }
       .subscribe({
-        response.write(it.encodePrettily())
+        response.write(it)
       }, {
         logger.warn("Unable to fetch activity feed for $userIdentifier because reasons.", it)
       }, {
-        response.close()
+        response.end()
       })
   }
   return router
