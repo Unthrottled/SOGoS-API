85a930147dafd8ef693ec38dfc651592909f27d7
DATBOI <alexsimons9999@gmail.com>
2019-09-23 12:12:05 -0500

    More able to get first activity before and after relative time

--- src/main/kotlin/io/acari/http/HistoryRoutes.kt
+++ src/main/kotlin/io/acari/http/HistoryRoutes.kt
@@ -6,31 +6,46 @@ import io.acari.util.loggerFor
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.vertx.core.json.Json
 import io.vertx.kotlin.core.json.jsonObjectOf
+import io.vertx.kotlin.ext.mongo.findOptionsOf
 import io.vertx.reactivex.core.Vertx
 import io.vertx.reactivex.ext.mongo.MongoClient
 import io.vertx.reactivex.ext.web.Router
+import io.vertx.reactivex.ext.web.RoutingContext
 import java.time.Instant
 import java.time.temporal.ChronoUnit
+import java.util.*
 
 private val logger = loggerFor("History Routes")
 
 const val JSON_STREAM = "application/stream+json"
+const val JSON = "application/json"
 
 fun createHistoryRoutes(vertx: Vertx, mongoClient: MongoClient): Router {
   val router = Router.router(vertx)
+  router.post("/:userIdentifier/first/before").handler { requestContext ->
+    val sortOrder = -1
+    val comparisonString = "\$lt"
+    handleFirstRequest(requestContext, mongoClient, sortOrder, comparisonString)
+  }
+  router.post("/:userIdentifier/first/after").handler { requestContext ->
+    val sortOrder = 1
+    val comparisonString = "\$gte"
+    handleFirstRequest(requestContext, mongoClient, sortOrder, comparisonString)
+  }
+
   router.get("/:userIdentifier/feed").handler { requestContext ->
     val request = requestContext.request()
     val userIdentifier = request.getParam("userIdentifier")
     val response = requestContext.response()
     val meow = Instant.now()
-    val from = try{
+    val from = try {
       request.getParam("from").toLong()
-    } catch (_: Throwable){
+    } catch (_: Throwable) {
       meow.minus(7, ChronoUnit.DAYS).toEpochMilli()
     }
-    val to = try{
+    val to = try {
       request.getParam("to").toLong()
-    } catch (_: Throwable){
+    } catch (_: Throwable) {
       meow.toEpochMilli()
     }
 
@@ -57,4 +72,44 @@ fun createHistoryRoutes(vertx: Vertx, mongoClient: MongoClient): Router {
       })
   }
   return router
+}
+
+private fun handleFirstRequest(
+  requestContext: RoutingContext,
+  mongoClient: MongoClient,
+  sortOrder: Int,
+  comparisonString: String
+) {
+  val request = requestContext.request()
+  val userIdentifier = request.getParam("userIdentifier")
+  val response = requestContext.response()
+  try {
+    val bodyAsJson = requestContext.bodyAsJson
+
+    val relativeTime = bodyAsJson.getLong("relativeTime")
+    if (relativeTime != null) {
+      mongoClient.rxFindWithOptions(
+        ActivityHistorySchema.COLLECTION, jsonObjectOf(
+          ActivityHistorySchema.GLOBAL_USER_IDENTIFIER to userIdentifier,
+          ActivityHistorySchema.TIME_OF_ANTECEDENCE to jsonObjectOf(
+            comparisonString to relativeTime
+          )
+        ),
+        findOptionsOf(sort = jsonObjectOf(ActivityHistorySchema.TIME_OF_ANTECEDENCE to sortOrder), limit = 1)
+      )
+        .map { it.firstOrNull() }
+        .filter(Objects::nonNull)
+        .subscribe({
+          response.putHeader(HttpHeaderNames.CONTENT_TYPE, JSON)
+          response
+            .setStatusCode(200)
+            .end(Json.encode(it))
+        }, {
+          response.setStatusCode(404).end()
+        })
+
+    }
+  } catch (e: Throwable) {
+    response.setStatusCode(400).end("""Expected "relativeTime" with a number of long type in request """)
+  }
 }
