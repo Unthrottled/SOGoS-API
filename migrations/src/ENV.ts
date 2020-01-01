e6f5d73adb591f0d930a8938ae3bba2c0ade7e7b
DATBOI <alexsimons9999@gmail.com>
2020-01-01 08:51:47 -0600

    Migration complete

---
+++ migrations/src/ENV.ts
@@ -0,0 +1,2 @@
+export const mongoUrl = 'mongodb://localhost:27017';
+export const HMAC_KEY = 'SomeSecret';
--- migrations/src/HashMigration.ts
+++ migrations/src/HashMigration.ts
@@ -1,1 +1,37 @@
-console.log('works')
+import {HMAC_KEY, mongoUrl} from "./ENV";
+import {MongoClient} from "mongodb";
+import {EffectSchema, UserSchema} from "../../nextGen/src/memory/Schemas";
+import crypto from "crypto";
+
+
+export const hashString = (value: string) =>
+  crypto.createHmac('SHA256', HMAC_KEY)
+    .update(value).digest('hex');
+
+new Promise((resolve, reject) =>
+  MongoClient.connect(mongoUrl, {useNewUrlParser: true}, ((error, result) => {
+  if (!error) {
+    const db = result.db('DEFAULT_DB');
+    const userCollection = db.collection(UserSchema.COLLECTION);
+    const effectCollection = db.collection(EffectSchema.COLLECTION);
+    const cursor = userCollection.find({}).stream();
+    cursor.on('data', doc => {
+      console.log(doc);
+      effectCollection.findOne({
+        [EffectSchema.GLOBAL_USER_IDENTIFIER]: doc[UserSchema.GLOBAL_USER_IDENTIFIER],
+        [EffectSchema.NAME]: 'USER_CREATED',
+      }, ((error1, result1) => {
+        if(!error1){
+          // userCollection.updateOne({
+          //   _id: doc._id
+          // }, {
+          //   $push: { identifiers: hashString(result1.content.email)}
+          // })
+        }
+      }))
+    });
+
+  }
+}))
+)
+
