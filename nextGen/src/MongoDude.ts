98cc2de5aa9b36acf943c86c1acde1a566047a3c
DATBOI <alexsimons9999@gmail.com>
2020-01-01 15:38:36 -0600

    Pointless caching is pointless

--- nextGen/src/MongoDude.ts
+++ nextGen/src/MongoDude.ts
@@ -3,27 +3,14 @@ import {Observable} from 'rxjs';
 
 const mongoUrl = 'mongodb://localhost:27017';
 
-let connection: MongoClient;
-
-export const getConnection = (): Observable<Db> => {
-  if (connection) {
-    return new Observable<Db>(
-      subscriber => {
-        subscriber.next(connection.db('DEFAULT_DB'));
+export const getConnection = (): Observable<Db> =>
+  new Observable<Db>(subscriber => {
+    MongoClient.connect(mongoUrl, {useNewUrlParser: true}, ((error, result) => {
+      if (error) {
+        subscriber.error(error);
+      } else {
+        subscriber.next(result.db('DEFAULT_DB'));
         subscriber.complete();
-      },
-    );
-  } else {
-    return new Observable<Db>(subscriber => {
-      MongoClient.connect(mongoUrl, {useNewUrlParser: true}, ((error, result) => {
-        if (error) {
-          subscriber.error(error);
-        } else {
-          connection = result;
-          subscriber.next(result.db('DEFAULT_DB'));
-          subscriber.complete();
-        }
-      }));
-    });
-  }
-};
+      }
+    }));
+  });
