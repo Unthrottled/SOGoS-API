4fee7357401c01aa065b15ccf7d2caf3406206da
DATBOI <alexsimons9999@gmail.com>
2020-01-03 05:25:51 -0600

    Should be able to find objective

--- nextGen/src/api/ObjectiveRoutes.ts
+++ nextGen/src/api/ObjectiveRoutes.ts
@@ -1,9 +1,33 @@
 import {Router} from 'express';
+import {throwIfEmpty} from 'rxjs/operators';
+import {ObjectiveHistorySchema} from '../memory/Schemas';
+import {NoResultsError} from '../models/Errors';
+import {APPLICATION_JSON} from '../routes/OpenRoutes';
+import {findOne} from '../rxjs/Convience';
 
 const objectivesRoutes = Router();
 
 objectivesRoutes.get('/:objectiveId', ((req, res) => {
-
+  const objectiveId = req.params.objectiveId;
+  findOne((db, mongoCallback) =>
+  db.collection(ObjectiveHistorySchema.COLLECTION)
+    .findOne({
+      [ObjectiveHistorySchema.IDENTIFIER]: objectiveId,
+    }, mongoCallback),
+  ).pipe(
+    throwIfEmpty(() => new NoResultsError()),
+  ).subscribe(objective => {
+    res.contentType(APPLICATION_JSON)
+      .status(200)
+      .send(objective);
+  }, error => {
+    if (error instanceof NoResultsError) {
+      res.status(404);
+    } else {
+      // todo log error
+      res.status(500);
+    }
+  });
 }));
 
 objectivesRoutes.post('/:objectiveId/complete', ((req, res) => {
