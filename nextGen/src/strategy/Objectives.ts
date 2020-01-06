da869b2f87f17f52303dedf859776ecb3d29d0d7
DATBOI <alexsimons9999@gmail.com>
2020-01-06 05:42:33 -0600

    Should be able to remove objective

--- nextGen/src/strategy/Objectives.ts
+++ nextGen/src/strategy/Objectives.ts
@@ -43,6 +43,8 @@ export interface Objective {
   iconCustomization: IconCustomization;
   associatedActivities: string[];
   categories: string[];
+  removalTime?: number;
+  completionTime?: number;
 }
 
 export interface CachedObjective {
@@ -55,11 +57,66 @@ export interface ObjectiveCacheEvent {
   userGUID: string;
 }
 
+const createOrUpdateObjectiveInHistory = (
+  objecto: Objective,
+  userIdentifier: string,
+) => performUpdate<Objective, any>(((db, callBackSupplier) => {
+  db.collection(ObjectiveHistorySchema.COLLECTION)
+    .replaceOne({
+      [ObjectiveHistorySchema.IDENTIFIER]: objecto.id,
+      [ObjectiveHistorySchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
+    }, objecto, {upsert: true}, callBackSupplier(objecto));
+}));
+
+const addUserIdentifier = (userIdentifier: string) => objecto => {
+  objecto[CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER] = userIdentifier;
+  return objecto;
+};
+
+function addDeletionAttribute(objecto: Objective, b: boolean) {
+  const timeOfAntecedence = objecto.antecedenceTime || rightMeow();
+  const attribute = b ? 'completionTime' : 'removalTime';
+  objecto[attribute] = timeOfAntecedence;
+  return objecto;
+}
+
 export const deleteObjective = (
   objective: Objective,
   userIdentifier: string,
 ): Observable<Objective> => {
-  return toObservable(objective);
+  return findOne(((db, mongoCallback) =>
+      db.collection(CurrentObjectiveSchema.COLLECTION)
+        .findOne({
+          [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
+        }, mongoCallback)
+  )).pipe(
+    mergeMap((storedObjectives: StoredObjectives) => {
+      const objectiveIds = storedObjectives.objectives;
+      const removalObjectiveId = objective.id;
+      if (objectiveIds.indexOf(removalObjectiveId) < 0) {
+        return toObservable(objective);
+      }
+      storedObjectives.objectives = objectiveIds.filter(id => id !== removalObjectiveId);
+      return performUpdate<Objective, any>(((db, callBackSupplier) =>
+        db.collection(CurrentObjectiveSchema.COLLECTION)
+          .replaceOne({
+            [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
+          }, storedObjectives, {upsert: true}, callBackSupplier(objective))
+      ));
+    }),
+    switchIfEmpty(performUpdate(((db, callBackSupplier) => {
+      const docs: StoredObjectives = {
+        guid: userIdentifier,
+        objectives: [],
+      };
+      db.collection(CurrentObjectiveSchema.COLLECTION)
+        .insertOne(docs, callBackSupplier(objective));
+    }))),
+    map(addUserIdentifier(userIdentifier)),
+    mergeMap(objecto =>
+      createOrUpdateObjectiveInHistory(addDeletionAttribute(objecto, true), userIdentifier)
+    ),
+  );
 };
 
 export const completeObjective = (
@@ -74,60 +131,52 @@ interface StoredObjectives {
   objectives: string[];
 }
 
-function performObjectivePersistence(userIdentifier: string, objective: Objective, effectName: string) {
-  return findOne((db, mongoCallback) =>
-    db.collection(CurrentObjectiveSchema.COLLECTION)
-      .findOne({
-        [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
-      }, mongoCallback),
-  ).pipe(
-    mergeMap((objectives: StoredObjectives) => {
-      if (objectives.objectives.find(id => id === objective.id)) {
-        return toObservable(objective);
-      } else {
-        objectives.objectives = getNewList(objectives.objectives, objective);
-        return performUpdate<Objective, any>(((db, callBackSupplier) => {
-          db.collection(CurrentObjectiveSchema.COLLECTION)
-            .replaceOne({
-              [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
-            }, objectives, {upsert: true}, callBackSupplier(objective));
-        }));
-      }
-    }),
-    switchIfEmpty(performUpdate(((db, callBackSupplier) => {
-      const docs: StoredObjectives = {
-        guid: userIdentifier,
-        objectives: [objective.id],
-      };
-      db.collection(CurrentObjectiveSchema.COLLECTION)
-        .insertOne(docs, callBackSupplier(objective));
-    }))),
-    map(objecto => {
-      objecto[CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER] = userIdentifier;
-      return objecto;
-    }),
-    mergeMap(objecto => {
+const performObjectivePersistence = (
+  userIdentifier: string,
+  objective: Objective,
+  effectName: string,
+) =>
+  findOne((db, mongoCallback) =>
+  db.collection(CurrentObjectiveSchema.COLLECTION)
+    .findOne({
+      [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
+    }, mongoCallback),
+).pipe(
+  mergeMap((objectives: StoredObjectives) => {
+    if (objectives.objectives.find(id => id === objective.id)) {
+      return toObservable(objective);
+    } else {
+      objectives.objectives = getNewList(objectives.objectives, objective);
       return performUpdate<Objective, any>(((db, callBackSupplier) => {
-        db.collection(ObjectiveHistorySchema.COLLECTION)
+        db.collection(CurrentObjectiveSchema.COLLECTION)
           .replaceOne({
-            [ObjectiveHistorySchema.IDENTIFIER]: objecto.id,
-            [ObjectiveHistorySchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
-          }, objecto, {upsert: true}, callBackSupplier(objecto));
+            [CurrentObjectiveSchema.GLOBAL_USER_IDENTIFIER]: userIdentifier,
+          }, objectives, {upsert: true}, callBackSupplier(objective));
       }));
-    }),
-    mergeMap(objecto => {
-      const meow = rightMeow();
-      return createEffect({
-        meta: {},
-        content: objecto,
-        name: effectName,
-        antecedenceTime: objecto.antecedenceTime || meow,
-        timeCreated: meow,
-        guid: userIdentifier,
-      }).pipe(map(_ => objecto));
-    }),
-  );
-}
+    }
+  }),
+  switchIfEmpty(performUpdate(((db, callBackSupplier) => {
+    const docs: StoredObjectives = {
+      guid: userIdentifier,
+      objectives: [objective.id],
+    };
+    db.collection(CurrentObjectiveSchema.COLLECTION)
+      .insertOne(docs, callBackSupplier(objective));
+  }))),
+  map(addUserIdentifier(userIdentifier)),
+  mergeMap(objecto => createOrUpdateObjectiveInHistory(objecto, userIdentifier)),
+  mergeMap(objecto => {
+    const meow = rightMeow();
+    return createEffect({
+      meta: {},
+      content: objecto,
+      name: effectName,
+      antecedenceTime: objecto.antecedenceTime || meow,
+      timeCreated: meow,
+      guid: userIdentifier,
+    }).pipe(map(_ => objecto));
+  }),
+);
 
 export const createObjective = (
   objective: Objective,
