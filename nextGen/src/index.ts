6361870e30cb940aa5a731a98506e2294155fe1d
DATBOI <alexsimons9999@gmail.com>
2020-01-07 05:54:14 -0600

    Chunked transfer not supported :(

--- nextGen/src/index.ts
+++ nextGen/src/index.ts
@@ -2,7 +2,7 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import {Cursor} from 'mongodb';
 import {Observable} from 'rxjs';
-import {mergeMap} from 'rxjs/operators';
+import {mergeMap, reduce} from 'rxjs/operators';
 import serverless from 'serverless-http';
 import {getConnection} from './Mongo';
 import authenticatedRoutes from './routes/AuthenticatedRoutes';
@@ -17,39 +17,6 @@ application.use(corsRequestHandler);
 application.use(corsErrorHandler);
 application.use(bodyParser.json({strict: false}));
 application.use(bodyParser.urlencoded({extended: true}));
-
-application.get('/test', (request, response) => {
-
-  response.writeHead(200, {
-    'Content-Type': 'application/json+stream',
-  });
-
-  response.write('[');
-  getConnection()
-    .pipe(
-      mergeMap(db => {
-        return new Observable(subscriber => {
-          const cursor: Cursor<any> = db.collection('user').find({})
-            .stream({
-              transform: document => ({
-                guid: document.guid,
-              }),
-            });
-          cursor.on('data', data => subscriber.next(data));
-          cursor.on('error', error => subscriber.error(error));
-          cursor.on('end', () => subscriber.complete());
-        });
-      }),
-    ).subscribe(item => {
-      response.write(`${JSON.stringify(item)},`);
-    }, error => {
-
-    }, () => {
-      response.write(']');
-      response.end();
-    },
-  );
-});
 
 // not secure
 application.use(openRoutes);
