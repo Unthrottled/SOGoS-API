c74392d28167e990d8213949c4ee698d2b3fdf94
DATBOI <alexsimons9999@gmail.com>
2020-01-08 06:12:06 -0600

    Working on getting logging set up

--- nextGen/src/Mongo.ts
+++ nextGen/src/Mongo.ts
@@ -1,12 +1,14 @@
+import chalk from 'chalk';
 import {Db, MongoClient} from 'mongodb';
 import {Observable} from 'rxjs';
 import {CONNECTION_STRING} from './ConfigurationENV';
+import {logger} from './utils/Utils';
 
 let connection: MongoClient;
 
 export const getConnection = (): Observable<Db> => {
   if (connection && connection.isConnected()) {
-    console.log('using cache');
+    logger.info(chalk.green('using cache'));
     return new Observable<Db>(
       subscriber => {
         subscriber.next(connection.db('DEFAULT_DB'));
@@ -14,7 +16,7 @@ export const getConnection = (): Observable<Db> => {
       },
     );
   } else {
-    console.log('new connection');
+    logger.info(chalk.red('new connection'));
     return new Observable<Db>(subscriber => {
       MongoClient.connect(CONNECTION_STRING, {useNewUrlParser: true}, ((error, result) => {
         if (error) {
--- nextGen/src/utils/Utils.ts
+++ nextGen/src/utils/Utils.ts
@@ -1,2 +1,19 @@
+import winston from 'winston';
+const { combine, timestamp, label, printf } = winston.format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+export const logger = winston.createLogger({
+  format: combine(
+    label({ label: 'right meow!' }),
+    timestamp(),
+    myFormat,
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
 
 export const rightMeow = () => new Date().valueOf();
