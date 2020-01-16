8d2ec55ce63a8e366132dfd67b343941dd260bc1
DATBOI <alexsimons9999@gmail.com>
2020-01-16 04:57:18 -0600

    Will need to convert this to rxjs for the next migration

--- migrations/src/HashMigration.ts
+++ migrations/src/HashMigration.ts
@@ -2,36 +2,46 @@ import {HMAC_KEY, mongoUrl} from "./ENV";
 import {MongoClient} from "mongodb";
 import {EffectSchema, UserSchema} from "../../nextGen/src/memory/Schemas";
 import crypto from "crypto";
-
+import Chalk from 'chalk';
 
 export const hashString = (value: string) =>
   crypto.createHmac('SHA256', HMAC_KEY)
     .update(value).digest('hex');
 
 new Promise((resolve, reject) =>
-  MongoClient.connect(mongoUrl, {useNewUrlParser: true}, ((error, result) => {
+  MongoClient.connect(mongoUrl, {useUnifiedTopology: true, useNewUrlParser: true}, ((error, result) => {
   if (!error) {
-    const db = result.db('DEFAULT_DB');
+    const db = result.db('sogos');
     const userCollection = db.collection(UserSchema.COLLECTION);
     const effectCollection = db.collection(EffectSchema.COLLECTION);
     const cursor = userCollection.find({}).stream();
     cursor.on('data', doc => {
       console.log(doc);
-      effectCollection.findOne({
-        [EffectSchema.GLOBAL_USER_IDENTIFIER]: doc[UserSchema.GLOBAL_USER_IDENTIFIER],
-        [EffectSchema.NAME]: 'USER_CREATED',
-      }, ((error1, result1) => {
-        if(!error1){
-          // userCollection.updateOne({
-          //   _id: doc._id
-          // }, {
-          //   $push: { identifiers: hashString(result1.content.email)}
-          // })
-        }
-      }))
+      // effectCollection.findOne({
+      //   [EffectSchema.GLOBAL_USER_IDENTIFIER]: doc[UserSchema.GLOBAL_USER_IDENTIFIER],
+      //   [EffectSchema.NAME]: 'USER_CREATED',
+      // }, ((error1, result1) => {
+      //   if(!error1){
+      //     const email = result1.content.email;
+      //     const identifiers = hashString(email);
+      //     console.log(`Adding ${identifiers} to email ${email}`);
+      //     // danger zone!
+      //     // userCollection.updateOne({
+      //     //   _id: doc._id
+      //     // }, {
+      //     //   $push: { identifiers: identifiers}
+      //     // }).then(()=> {
+      //     //   console.log(`Wrote ${identifiers} to email ${email}`);
+      //     // })
+      //   }
+      // }))
     });
-
+    cursor.on('end', ()=>{
+      result.close(()=>resolve())
+    })
   }
 }))
-)
+).then(()=>{
+  console.info(Chalk.green('Something something something, complete.'))
+});
 
