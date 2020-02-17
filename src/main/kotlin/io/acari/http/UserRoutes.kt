8daf7ef5d78cb8a257f5baa3c67af1de1d331233
DATBOI <alexsimons9999@gmail.com>
2020-02-17 14:10:04 -0600

    Dispatch all the events!

--- src/main/kotlin/io/acari/http/UserRoutes.kt
+++ src/main/kotlin/io/acari/http/UserRoutes.kt
@@ -1,14 +1,19 @@
 package io.acari.http
 
+import io.acari.memory.Effect
+import io.acari.memory.user.EFFECT_CHANNEL
+import io.acari.security.USER_IDENTIFIER
 import io.acari.user.UserService
 import io.acari.util.loggerFor
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.netty.handler.codec.http.HttpHeaderValues
 import io.vertx.core.Handler
+import io.vertx.core.json.JsonObject
 import io.vertx.reactivex.core.Vertx
 import io.vertx.reactivex.ext.mongo.MongoClient
 import io.vertx.reactivex.ext.web.Router
 import io.vertx.reactivex.ext.web.RoutingContext
+import java.time.Instant
 
 private val logger = loggerFor("UserRoutes")
 
@@ -24,36 +29,78 @@ fun createUserHandler(userService: UserService): Handler<RoutingContext> = Handl
     }
 }
 
+const val USER_WELCOMED = "USER_WELCOMED"
+const val TACMOD_NOTIFIED = "TACMOD_NOTIFIED"
+const val TACMOD_DOWNLOADED = "TACMOD_DOWNLOADED"
+const val TACMOD_THANKED = "TACMOD_THANKED"
+
 fun createOnboardingRouter(vertx: Vertx, mongoClient: MongoClient): Router {
   val router = Router.router(vertx)
-  router.post("/welcomed").handler {
-    routingContext ->
-
-    // update user with new attributes
-    TODO("Need to be implemented.")
+  router.post("/welcomed").handler { requestContext ->
+    val timeCreated = Instant.now().toEpochMilli()
+    val userIdentifier = requestContext.request().headers().get(USER_IDENTIFIER)
+    vertx.eventBus().publish(
+      EFFECT_CHANNEL, Effect(
+        userIdentifier,
+        timeCreated,
+        timeCreated,
+        USER_WELCOMED,
+        JsonObject(),
+        extractValuableHeaders(requestContext)
+      )
+    )
+    requestContext.response().setStatusCode(201).end()
   }
 
-  router.post("/TacMod/notify").handler {
-      routingContext ->
+  router.post("/TacMod/notify").handler { requestContext ->
+    val timeCreated = Instant.now().toEpochMilli()
+    val userIdentifier = requestContext.request().headers().get(USER_IDENTIFIER)
+    vertx.eventBus().publish(
+      EFFECT_CHANNEL, Effect(
+        userIdentifier,
+        timeCreated,
+        timeCreated,
+        TACMOD_NOTIFIED,
+        JsonObject(),
+        extractValuableHeaders(requestContext)
+      )
+    )
+    requestContext.response().setStatusCode(201).end()
 
-    // update user with new attributes
-    TODO("Need to be implemented.")
   }
 
-  router.post("/TacMod/downloaded").handler {
-      routingContext ->
+  router.post("/TacMod/downloaded").handler { requestContext ->
+    val timeCreated = Instant.now().toEpochMilli()
+    val userIdentifier = requestContext.request().headers().get(USER_IDENTIFIER)
+    vertx.eventBus().publish(
+      EFFECT_CHANNEL, Effect(
+        userIdentifier,
+        timeCreated,
+        timeCreated,
+        TACMOD_DOWNLOADED,
+        JsonObject(),
+        extractValuableHeaders(requestContext)
+      )
+    )
+    requestContext.response().setStatusCode(201).end()
 
-    // update user with new attributes
-    TODO("Need to be implemented.")
   }
 
-  router.post("/TacMod/thanked").handler {
-      routingContext ->
-
-    // update user with new attributes
-    TODO("Need to be implemented.")
+  router.post("/TacMod/thanked").handler { requestContext ->
+    val timeCreated = Instant.now().toEpochMilli()
+    val userIdentifier = requestContext.request().headers().get(USER_IDENTIFIER)
+    vertx.eventBus().publish(
+      EFFECT_CHANNEL, Effect(
+        userIdentifier,
+        timeCreated,
+        timeCreated,
+        TACMOD_THANKED,
+        JsonObject(),
+        extractValuableHeaders(requestContext)
+      )
+    )
+    requestContext.response().setStatusCode(201).end()
   }
-
 
   return router
 }
