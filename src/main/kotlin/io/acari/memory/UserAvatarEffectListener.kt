edd901f2d14dd42c09b64ee974791cb510774895
DATBOI <alexsimons9999@gmail.com>
2020-03-03 18:31:36 -0600

    More able to record profile information

---
+++ src/main/kotlin/io/acari/memory/UserAvatarEffectListener.kt
@@ -0,0 +1,52 @@
+package io.acari.memory
+
+import io.acari.http.AVATAR_UPLOADED
+import io.acari.http.AVATAR_UPLOAD_REQUESTED
+import io.acari.memory.user.UserMemoryWorkers
+import io.acari.util.toOptional
+import io.vertx.core.Handler
+import io.vertx.kotlin.core.json.jsonObjectOf
+import io.vertx.reactivex.core.Vertx
+import io.vertx.reactivex.core.eventbus.Message
+import io.vertx.reactivex.ext.mongo.MongoClient
+import java.util.*
+
+
+class UserAvatarEffectListener(private val mongoClient: MongoClient, private val vertx: Vertx) :
+  Handler<Message<Effect>> {
+  companion object {
+    private val miscUserMappings =
+      mapOf(
+        AVATAR_UPLOADED to true,
+        AVATAR_UPLOAD_REQUESTED to true
+      )
+  }
+
+
+  override fun handle(message: Message<Effect>) {
+    val effect = message.body()
+    extractUpdateType(effect)
+      .ifPresent {
+        mongoClient.rxFindOneAndUpdate(
+            UserSchema.COLLECTION,
+            jsonObjectOf(UserSchema.GLOBAL_USER_IDENTIFIER to effect.guid),
+            jsonObjectOf(
+              "\$set" to jsonObjectOf(
+                "misc.avatarUploaded" to true
+              )
+            )
+          )
+          .subscribe({}) {
+            UserMemoryWorkers.log.warn("Unable to update user avatar attributes for raisins -> ", it)
+          }
+      }
+  }
+
+  private fun extractUpdateType(effect: Effect): Optional<Boolean> {
+    return effect.toOptional()
+      .filter { miscUserMappings.containsKey(effect.name) }
+      .map {
+        miscUserMappings[effect.name]
+      }
+  }
+}
--- src/main/kotlin/io/acari/memory/user/UserMemoryWorkers.kt
+++ src/main/kotlin/io/acari/memory/user/UserMemoryWorkers.kt
@@ -1,9 +1,6 @@
 package io.acari.memory.user
 
-import io.acari.memory.EffectListener
-import io.acari.memory.SOGoSUserEffectListener
-import io.acari.memory.TacModEffectListener
-import io.acari.memory.UserSharedEffectListener
+import io.acari.memory.*
 import io.acari.util.loggerFor
 import io.reactivex.Completable
 import io.vertx.reactivex.core.Vertx
@@ -21,6 +18,7 @@ object UserMemoryWorkers {
     eventBus.consumer(EFFECT_CHANNEL, SOGoSUserEffectListener(mongoClient, vertx))
     eventBus.consumer(EFFECT_CHANNEL, TacModEffectListener(mongoClient, vertx))
     eventBus.consumer(EFFECT_CHANNEL, UserSharedEffectListener(mongoClient, vertx))
+    eventBus.consumer(EFFECT_CHANNEL, UserAvatarEffectListener(mongoClient, vertx))
     return Completable.complete()
   }
 }
